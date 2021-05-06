cc9138d1c948b16d5c10ad5dd93e8e543f4c5786
gmono <gaozijian0411@foxmail.com>
2021-05-06 13:46:24 +0800

    添加elementui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,9 @@
 <template>
   <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <el-button>testabcd</el-button>
+  <el-input type="textarea" v-model="text" placeholder="输入" :rows="10" ></el-input>
+  <el-button type="primary">执行</el-button>
+  <div>{{count}}</div>
 </template>
 
 <script lang="ts">
@@ -46,7 +18,8 @@ export default defineComponent({
   },
   setup: () => {
     const count = ref(0)
-    return { count }
+    const text=ref("")
+    return { count,text }
   }
 })
 </script>
